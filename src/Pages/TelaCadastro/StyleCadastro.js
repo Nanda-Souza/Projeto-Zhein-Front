a55fc0b8af62c899a4e089023f197ddeea9fc4db
Nanda-Souza <fesouoli@gmail.com>
2023-01-27 02:23:26 -0300

    feat: Integracao com a API de cadastro

--- src/Pages/TelaCadastro/StyleCadastro.js
+++ src/Pages/TelaCadastro/StyleCadastro.js
@@ -70,3 +70,4 @@ export const Tela = styled.div`
     cursor: pointer;
   }
 `;
+
--- src/Pages/TelaCadastro/TelaCadastro.js
+++ src/Pages/TelaCadastro/TelaCadastro.js
@@ -1,21 +1,56 @@
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../Assets/logo.png";
-import { Tela, Form } from "./StyleCadastro";
+import { Tela, Form} from "./StyleCadastro";
 import { Container } from "../../Components/Container";
 import { useState } from "react";
+//import { ThreeDots } from "react-loader-spinner";
+import axios from "axios";
 
 export default function Login() {
+  //const [loading, setLoading] = useState(false)
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
-  const [confirmesenha, setConfirmeSenha] = useState("");
+  const [confirmeSenha, setConfirmeSenha] = useState("");
   const navigate = useNavigate();
+
+  const envioRegistro = async (e) => {
+    e.preventDefault()
+
+    const dadosCadastro = {nome, email, senha, confirmeSenha}
+    //setLoading(true)
+
+    try{    
+
+    await axios.post(`${process.env.REACT_APP_API_URL}/cadastro`, dadosCadastro)
+    
+    } catch (err){
+        if (err.response?.status === 422){
+            alert("Senha e confirmação de senha devem ser iguais!")
+        } else if (err.response?.status === 409){
+            alert("Usuário ou email já cadastrados!")
+        } else {
+            alert("Não foi possível fazer o cadastro, favor tentar novamente mais tarde!")
+        }        
+        //setLoading(false)
+        return;
+    }    
+    
+    //setLoading(false)
+    navigate("/")
+    
+    
+    
+
+}
+
 
   return (
     <Container>
       <Tela>
         <img src={logo} alt="logo" />
-        <Form>
+
+    <Form onSubmit={envioRegistro}>
         <input
             type="nome"
             placeholder="Nome"
@@ -41,13 +76,18 @@ export default function Login() {
             type="password"
             placeholder="Confirme a senha"
             required
-            value={confirmesenha}
+            value={confirmeSenha}
             onChange={(e) => setConfirmeSenha(e.target.value)}
           />
+
+
           <button type="submit">
             <strong>Cadastrar</strong>
           </button>
-        </Form>
+          
+
+    </Form>
+
         <br />
         <Link to="/">
           <p>Já possui conta? Faça login!</p>
