9abff433cf7a591bedda46684cf7a00279d1a859
Nanda-Souza <fesouoli@gmail.com>
2023-01-29 09:17:07 -0300

    feat: Integrar tela_home com a API get-produtos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/TelaLogin/TelaLogin";
 import Cadastro from "./Pages/TelaCadastro/TelaCadastro";
+import Home from "./Pages/TelaHome/TelaHome"
 import Pagamento from "./Pages/TelaPagamento/TelaPagamento";
 import Carrinho from "./Pages/TelaCarrinho/CarrinhoPages"
+
 
 function App() {
   return (
@@ -10,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/home" element={<Home />}/>
         {/* 
-        <Route path="/home" element={}/>
         <Route path="/:id_do_produto" element={}/> */}
         <Route path="/carrinho" element={<Carrinho />}/>
         <Route path="/pagamento" element={<Pagamento />} />
