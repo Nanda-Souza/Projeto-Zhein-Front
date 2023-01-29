0e43054f17d4cb92ed9469286493caa5b0a7ae78
taianaazevedo <taianazev@gmail.com>
2023-01-29 19:44:20 -0300

    feat: função login ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import UserContext from "./Context/UserContext";
 import Login from "./Pages/TelaLogin/TelaLogin";
 import Cadastro from "./Pages/TelaCadastro/TelaCadastro";
-import Home from "./Pages/TelaHome/TelaHome"
+import Home from "./Pages/TelaHome/TelaHome";
 import Pagamento from "./Pages/TelaPagamento/TelaPagamento";
 import Carrinho from "./Pages/TelaCarrinho/CarrinhoPages";
 import Sucesso from "./Pages/TelaSucesso/TelaSucesso";
 
-
 function App() {
+  const [usuarioLogado, setUsuarioLogado] = useState(null);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/home" element={<Home />}/>
-        {/* 
+    <UserContext.Provider value={{ usuarioLogado, setUsuarioLogado }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/home" element={<Home />} />
+          {/* 
         <Route path="/:id_do_produto" element={}/> */}
-        <Route path="/carrinho" element={<Carrinho />} />
-        <Route path="/pagamento" element={<Pagamento />} />
-        <Route path="/sucesso" element={<Sucesso />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/carrinho" element={<Carrinho />} />
+          <Route path="/pagamento" element={<Pagamento />} />
+          <Route path="/sucesso" element={<Sucesso />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
